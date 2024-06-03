1d7870915ca1dd5884680f71e2ff8ae3aff6207a
NekruzAsh <nekruzashrapov16@gmail.com>
2024-06-03 18:51:26 -0400

    First commit

---
+++ contracts/Calculator.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.25;
+
+contract Calculator {
+
+    uint256 result = 0;
+
+    function add(uint256 num) public {
+         result += num;
+    }
+
+    function subtract(uint256 num) public {
+        result -= num;
+    }
+
+    function multiply(uint256 num) public {
+        result *= num;
+    }
+
+    function divide(uint256 num) public {
+        result /= num;
+    }
+
+    function get() public view returns (uint256) {
+        return result;
+    }
+}
